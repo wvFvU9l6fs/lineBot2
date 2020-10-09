e0a1f313a552cede5e8c7ba099a12493067e74e8
wvFvU9l6fs <72595839+wvFvU9l6fs@users.noreply.github.com>
2020-10-09 13:51:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def index():
     except Exception:
         return "Could not read to file"
     
-    return "นายอาคม สุวรรณประเสริฐ เลขที่ 0 ชั้น ม.4/"
+    return "นายสุวพัชร ต่อพัฒนนันท์ ม.4/15 "
 
 @app.route("/webhook", methods=['POST'])
 def webhook():
